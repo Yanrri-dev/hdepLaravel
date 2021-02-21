7f1770d04e47449630b551628fd19f412bbf38e7
Yanrri-dev <nicrojas16@alumnos.utalca.cl>
2021-02-21 20:12:36 -0300

    Update vista show
    -Se formateo correctamente la tabla que muestra la pauta de evaluación en la vista show.blade.php
    -Falta agregar estilos a la tabla, funcionalidad para descarga en pdf, csv o xls

--- resources/views/evaluations/show.blade.php
+++ resources/views/evaluations/show.blade.php
@@ -10,21 +10,47 @@
             </tr>
             <tr>
                 <th>Nombre Estudiante</th>
+                @php
+                    $puntaje_total = 0;
+                @endphp
                 @foreach ($criterios as $c)
                     @php
                         $num_criterios++;
+                        $puntaje_total += $c->score;
                     @endphp
                     <th>{{$c->criterio_name}} <br> ({{$c->score}} puntos)</th>
                 @endforeach
+                <th>Nota</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($participantes as $p)
                 @if ($i % $num_criterios == 0)
                     <tr>
-                    <td>{{$p->name}} {{$p->last_name}}</td>
+                        <td>{{$p->name}} {{$p->last_name}}</td>
                 @endif
-                    </tr>
+                        <td>{{$p->score_obtenido}}</td>
+                        @php
+                            $suma = $suma + $p->score_obtenido;
+                        @endphp
+                        @if (($i+1) % $num_criterios == 0)
+                            @php
+                                $puntaje_corte = $puntaje_total * 0.6;
+                                $diferencia = $puntaje_total - $puntaje_corte; 
+                            @endphp
+                            @if ($suma < $puntaje_corte)
+                                @php
+                                    $nota = ((3* $suma)/$puntaje_corte) +1;
+                                @endphp
+                            @else
+                                @php
+                                    $nota = ((3*($suma - $puntaje_corte))/$diferencia) + 4 ;
+                                @endphp                                
+                            @endif
+                        
+                            <td>{{round($nota,2)}}</td>
+                            </tr>
+                        @endif
                 @php
                     $i++;
                 @endphp
